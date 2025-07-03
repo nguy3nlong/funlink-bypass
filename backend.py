90e65615cf53ab50f5f9e53eb5e1e1e4e20f8fcb
Reno <longnguyenstocks@gmail.com>
2025-07-03 20:40:17 +0700

    Update backend.py

--- backend.py
+++ backend.py
@@ -139,13 +139,3 @@ def c():
             return jsonify({'error': 'failed'}), 400
     else:
         return jsonify({'error': 'failed'}), 400
-
-
-    
-
-
-
-if __name__ == "__main__":
-    
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
